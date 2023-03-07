0d8580b0fb89c03a44a503146bdb8d225b433ea8
Anurag Agarwal <anuragagarwal530@gmail.com>
2023-03-07 15:42:27 +0530

    first commit on cocktail db

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='about-section section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quo voluptatibus nostrum iste id beatae, perspiciatis numquam rerum reprehenderit cupiditate voluptatum asperiores eius sapiente mollitia sint consequuntur tempore necessitatibus libero quibusdam illo voluptate suscipit tempora soluta. Neque et molestiae aliquam.</p>
+    </section>
+  )
+}
+
+export default About
